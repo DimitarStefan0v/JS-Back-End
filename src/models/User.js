c7ec82645170ef2d5d089a3b9893b795d4313919
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-06-12 07:20:04 +0300

    Hash user password before save in db

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
@@ -11,6 +13,12 @@ userSchema.virtual('repeatPassword')
             throw new mongoose.MongooseError('Passwords don\'t match');
         }
     });
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+});
 
 const User = mongoose.model('User', userSchema);
 
