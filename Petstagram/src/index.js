017f690d75beb5a4725beb6d1eb166298814bd59
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-06-16 09:31:03 +0300

    Add routes

--- Petstagram/src/index.js
+++ Petstagram/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
+const routes = require('./routes');
 
 const app = express();
 
 const PORT = 5000;
 
 expressConfig(app);
+app.use(routes);
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
