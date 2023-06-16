e9069750715164239582896758a31bdf4261f5fd
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-06-16 12:37:43 +0300

    Validate if the username is already taken

---
+++ Petstagram/src/managers/userManager.js
@@ -0,0 +1,18 @@
+const User = require('../models/User');
+
+exports.login = (username, password) => {
+
+};
+
+exports.register = async (userData) => {
+    const user = await User.findOne({ username: userData.username });
+    if (user) {
+        throw new Error('Username already exists');
+    }
+
+    return User.create(userData);
+};
+
+exports.logout = () => {
+
+};
