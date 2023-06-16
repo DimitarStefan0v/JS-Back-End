4b06355bea3628b89d594c4221d8cd6c0c189efb
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-06-16 12:36:31 +0300

    Validate that password is the same as repeat password

--- Petstagram/src/models/User.js
+++ Petstagram/src/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
         type: String,
@@ -14,6 +16,14 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 });
+
+userSchema.virtual('repeatPassword')
+    .set(function(value) {
+        if (this.password !== value) {
+            throw new Error('Passwords don\'t match!');
+        }
+    });
+
 
 const User = mongoose.model('User', userSchema);
 
